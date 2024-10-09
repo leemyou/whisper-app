4c0ea920e0a436588006cd35f82ed90898e35b42
leemyou <limhyeon0809@gmail.com>
2024-10-09 16:31:15 +0900

    FIRE : 기본 컴포넌트 제거
    - 필요없는 기본 컴포넌트 제거
    - 사용하지 않는 폰트 제거

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,11 @@
-import { Link, Stack } from "expo-router";
-import { StyleSheet } from "react-native";
-
-import { ThemedView } from "@/components/ThemedView";
+import { Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
-    <>
+    <View>
       <Stack.Screen options={{ title: "Oops!" }} />
-      {/* <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView> */}
-    </>
+    </View>
   );
 }
 
