84b250223b8c40db9ff1cd39031d36b54d424cc3
leemyou <limhyeon0809@gmail.com>
2024-10-10 20:46:02 +0900

    STYLE : 사진달력 컴포넌트 구현
    - 사진달력 UI 구현
    - 클릭 시 기능 구현
    - home에 테스트 코드 추가

---
+++ components/calendars/PictureCalender.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Image, Pressable, StyleSheet, View } from "react-native";
+import type { Dayjs } from "dayjs";
+import { BasicTextInput } from "../inputs/BasicTextInput";
+import { getCalendarWithImages } from "@/utils/date";
+
+type PictureCalenderProps = {
+  date: Dayjs;
+  imgDataArr: { date: string; imgUrl: string }[];
+  onPressDay?: (day: number) => void;
+};
+
+export const PictureCalender = ({
+  date,
+  imgDataArr,
+  onPressDay,
+}: PictureCalenderProps) => {
+  const weekOfDay = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  const monthOfDays = getCalendarWithImages(date, imgDataArr);
+
+  return (
+    <View style={pcCalenderStyle.mainContainer}>
+      {/* 요일 View */}
+      <View style={pcCalenderStyle.weekDay}>
+        {weekOfDay.map((value, index) => (
+          <BasicTextInput
+            size="footnote"
+            color={index === 0 ? "main" : index === 6 ? "blue" : "text"}
+            style={{ flex: 1, textAlign: "center" }}
+          >
+            {value}
+          </BasicTextInput>
+        ))}
+      </View>
+
+      {/* 달력 View */}
+      <View style={pcCalenderStyle.calenderContainer}>
+        {monthOfDays.map((week) => {
+          return (
+            <View style={pcCalenderStyle.calendarWeek}>
+              {week.map((dateValue, dayIndex) => (
+                <Pressable
+                  style={pcCalenderStyle.calendarDate}
+                  onPress={() => {
+                    if (dateValue.day !== null && onPressDay) {
+                      onPressDay(dateValue.day);
+                    }
+                  }}
+                >
+                  {/* TODO : 배경이미지가 없다는 조건도 추후 추가해줘야함 */}
+                  {dateValue.day === null || !dateValue.imgUrl ? (
+                    <></>
+                  ) : (
+                    <Image
+                      source={require("@/assets/images/test.png")}
+                      style={pcCalenderStyle.calendarImg}
+                    />
+                  )}
+
+                  <BasicTextInput
+                    weight="light"
+                    color={
+                      dayIndex % 7 === 0
+                        ? "main"
+                        : dayIndex % 6 === 0
+                        ? "blue"
+                        : "text"
+                    }
+                    style={[
+                      pcCalenderStyle.calendarText,
+                      dateValue.day === null && { display: "none" },
+                    ]}
+                  >
+                    {dateValue.day}
+                  </BasicTextInput>
+                </Pressable>
+              ))}
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+const pcCalenderStyle = StyleSheet.create({
+  mainContainer: {},
+  weekDay: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#f2f2f2",
+    columnGap: 8,
+    marginBottom: 8,
+  },
+  calenderContainer: {
+    width: "100%",
+  },
+  calendarWeek: {
+    width: "100%",
+    flexDirection: "row",
+    columnGap: 8,
+    marginBottom: 12,
+    alignItems: "stretch",
+  },
+  calendarDate: {
+    flex: 1,
+    height: 60,
+    borderRadius: 8,
+    overflow: "hidden",
+    position: "relative",
+  },
+  calendarImg: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    objectFit: "cover",
+    top: 0,
+    left: 0,
+  },
+  calendarText: {
+    fontSize: 20,
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+    lineHeight: 60,
+    backgroundColor: "rgba(255,255,255,0.5)",
+  },
+});
