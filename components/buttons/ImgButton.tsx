7e8609bebc51ff122664b51353151bf270d3b04e
leemyou <limhyeon0809@gmail.com>
2024-10-14 16:46:51 +0900

    STYLE : 이미지 추가 버튼 UI
    - 이미지 추가 버튼 UI 구성

---
+++ components/buttons/ImgButton.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Pressable,
+  PressableProps,
+  StyleProp,
+  StyleSheet,
+  ViewProps,
+} from "react-native";
+import ImgIcon from "@/assets/images/icons/solar--gallery-round-linear.svg";
+import { Colors } from "@/constants/Colors";
+import { BasicTextInput } from "../inputs/BasicTextInput";
+
+type ImgButtonProps = Omit<PressableProps, "style"> & {
+  style?: StyleProp<ViewProps>;
+};
+
+export const ImgButton = ({ style, ...rest }: ImgButtonProps) => {
+  return (
+    <Pressable style={[IbStyle.btn, style]} {...rest}>
+      <ImgIcon width={52} height={52} color={Colors.light.sub} />
+      <BasicTextInput color="sub" weight="bold" size="subTitle">
+        사진을 추가해주세요.
+      </BasicTextInput>
+    </Pressable>
+  );
+};
+
+const IbStyle = StyleSheet.create({
+  btn: {
+    width: 300,
+    height: 300,
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: Colors.light.sub,
+    borderStyle: "dashed",
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap: 16,
+  },
+});
